9acd412bdd204f9915d9b28367abbbe06b9f3046
Florence <plorenznl@gmail.com>
2024-10-01 01:18:32 +0800

    Game Window Values

---
+++ Snake_remake.py
@@ -0,0 +1,15 @@
+import tkinter
+import random
+
+ROWS = 30
+COLS - 30
+TILE_SIZE = 30
+
+WINDOW_WIDTH = TILE_SIZE * COLS   # 30*30 = 900
+WINDOW_HEIGHT = TILE_SIZE * ROWS  # 30*30 = 900
+
+
+class Tile:
+    def __init__(self, x, y)
+        self.x = x
+        self.y = y
