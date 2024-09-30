b723a0ff9b7746b76703afb85198646308d4f0e4
Florence <plorenznl@gmail.com>
2024-10-01 02:24:51 +0800

    Game Window

--- Snake_remake.py
+++ Snake_remake.py
@@ -10,6 +10,17 @@ WINDOW_HEIGHT = TILE_SIZE * ROWS  # 30*30 = 900
 
 
 class Tile:
-    def __init__(self, x, y)
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+# game window
+window = tkinter.Tk()
+window.title("The Snake Game")
+window.resizable(False, False)
+
+canvas = tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, borderwidth=0
+                        highlightthickness=0)
+canvas.pack()
+window.update()
