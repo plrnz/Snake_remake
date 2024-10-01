23ff6f9066a482d1d644854f813499d910ecca9e
Florence <plorenznl@gmail.com>
2024-10-01 13:01:28 +0800

    Collision

--- Snake_remake.py
+++ Snake_remake.py
@@ -89,3 +89,9 @@ def move():
         game_over = True
         return
     
+    # collision
+    if (snake.x == food.x and snake.y == food.y):
+        snake_body.append(Tile(food.x, food.y))
+        food.x = random.randint(0, COLS - 1) * TILE_SIZE
+        food.y = random.randint(0, ROWS - 1) * TILE_SIZE
+        score += 1
