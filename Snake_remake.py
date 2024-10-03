7276c446c80385e6dd4266e5371656382188fbd7
Florence <plorenznl@gmail.com>
2024-10-04 00:33:51 +0800

    Game Window

--- Snake_remake.py
+++ Snake_remake.py
@@ -13,3 +13,14 @@ class Tile:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+
+# game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+canvas = tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT, borderwidth=0,
+                        highlightthickness=0)
+canvas.pack()
+window.update()
