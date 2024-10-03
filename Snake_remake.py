3038f7633f8573b300c15d5e5401aac0913c8aa3
Florence <plorenznl@gmail.com>
2024-10-04 00:39:28 +0800

    Center the Window

--- Snake_remake.py
+++ Snake_remake.py
@@ -24,3 +24,12 @@ canvas = tkinter.Canvas(window, bg="black", width=WINDOW_WIDTH, height=WINDOW_HE
                         highlightthickness=0)
 canvas.pack()
 window.update()
+
+# center the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width / 2) - (window_width / 2))
+window_y = int((screen_height / 2) - (window_height / 2))
